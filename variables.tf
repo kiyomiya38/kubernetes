0af501e1b608f2026efb97e07bdcfc8ceaf2f10c
kiyomiya38 <kiyomiyayuuki@gmail.com>
2025-03-09 17:30:50 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "aws_region" {
-  default = "ap-northeast-1"
+  default = "ap-northeast-3"
 }
 
 variable "ami_id" {
-  default = "ami-0ac6b9b2908f3e20d" # 必要に応じて変更
+  default = "ami-0a0bcba223270ed99" # 必要に応じて変更
 }
 
 variable "key_name" {
-  default = "k8s-key" # AWSに登録済みのSSHキー
+  default = "test-key" # AWSに登録済みのSSHキー
 }
 
 variable "security_group_id" {
-  default = "sg-06402ee46c42f8e96" # 適用するセキュリティグループID
+  default = "sg-08b2d9099b3ba1340" # 適用するセキュリティグループID
 }
